ac85e997d9d5c8136e311e9c54d6f09f77171f2c
Aman .. <bhaskarshah4235@gmail.com>
2024-07-11 23:42:19 +0530

    1st program of if else in cpp

---
+++ LearnC++.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "enter age : ";
+    cin >> n;
+
+    if(n<18 && n>0){
+        if(n>=18 && n>=14);
+        cout << "You're teen";
+    cout << "You're young";
+    }
+    else{
+        cout << "You're adult";
+    }
+    return 0;
+
+}
