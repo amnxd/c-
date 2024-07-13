d9b931c9e7962f6917c97ce9f92c30c64164d1ab
Aman .. <bhaskarshah4235@gmail.com>
2024-07-13 13:31:41 +0530

    functions

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void -> which doesn't returns anything
+// return
+// parameterised
+// non paramerterised 
+
+void printName(string name){  //void fn
+    cout << "hey " << name << endl;
+}
+int main(){
+    string name;
+    cin >> name;
+    printName(name);
+    return 0;
+}
