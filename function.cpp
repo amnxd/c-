2139035fe882a7f613f912138ffd717571e914f4
Aman .. <bhaskarshah4235@gmail.com>
2024-07-13 13:59:08 +0530

    functions , passed by ref

--- function.cpp
+++ function.cpp
@@ -6,12 +6,38 @@ using namespace std;
 // parameterised
 // non paramerterised 
 
-void printName(string name){  //void fn
-    cout << "hey " << name << endl;
+// void printName(string name){  //void fn
+//     cout << "hey " << name << endl;
+// }
+// int main(){
+//     string name;
+//     cin >> name;
+//     printName(name);
+//     return 0;
+// }
+
+// void dosmtg(string &s){  // "&" is used to pass by reference
+//     s[0]= 'D';
+//     cout << s << endl;
+// }
+// int main(){
+//     string s = "Aman";
+//     dosmtg(s);
+//     cout << s << endl;
+//     return 0;
+// }
+
+void doarry(int arr[], int n){ // array pass by ref by default
+    arr[0] = 100;
+    cout << "Value inside function: " << arr[0] << endl;
 }
 int main(){
-    string name;
-    cin >> name;
-    printName(name);
+    int n = 2;
+    int arr[n];
+    for(int i = 0;i<n; i = i + 1){
+        cin >> arr[i];
+    }
+    doarry(arr,n);
+    cout << "value inside int main: " << arr[0] << endl;
     return 0;
 }
