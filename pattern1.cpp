b9a6f04b258b3fd2526f81069c305b41d51d5f7e
Aman <bhaskarshah4235@gmail.com>
2024-07-15 11:09:23 +0530

    lskabijdb

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,10 +1,4 @@
 // patterns - nested loops
-
-// * * * *
-// * * * *
-// * * * * 
-// * * * *
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -21,8 +15,21 @@ void pattern1(int n){
     }
 }
 
+void pattern2(int n){
+    // * 
+    // * * 
+    // * * * 
+    // * * * *
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     int n;
     cin >> n;
-    pattern1(n); 
+    pattern2(n); 
 }
