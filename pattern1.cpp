73dc4ce0988f98301eea4446413311e238c5ff92
Aman <bhaskarshah4235@gmail.com>
2024-07-16 22:35:05 +0530

    jkadvus

--- pattern1.cpp
+++ pattern1.cpp
@@ -14,7 +14,6 @@ void pattern1(int n){
         cout << endl;
     }
 }
-
 void pattern2(int n){
     // * 
     // * * 
@@ -27,9 +26,89 @@ void pattern2(int n){
         cout << endl;
     }
 }
+void pattern3(int n){
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern4(int n){
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=i; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern5(int n){
+    // * * * * *
+    // * * * *
+    // * * *
+    // * *
+    // *
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<n-i+1; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void pattern6(int n){
+    // 1 2 3 4 5
+    // 1 2 3 4
+    // 1 2 3
+    // 1 2
+    // 1
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern7(int n){
+    //     *    
+    //    ***
+    //   *****
+    //  *******
+    // *********
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        // star
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+}
+
 
 int main(){
+    // int t;
+    // cin >> t;
+    // for(int i=0; i<t;i++){
+    //     int n;
+    //     cin >> n;
+    //     // pattern1(n);
+    //     // pattern2(n);
+    //     // pattern3(n);
+    //     // pattern4(n);
+    //     // pattern5(n);
+    //     // pattern6(n);
+    //     pattern7(n);
+    // }
     int n;
     cin >> n;
-    pattern2(n); 
+    pattern7(n);
+    return 0;
 }
