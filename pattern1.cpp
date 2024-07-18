3bd7702ab29cf6ca9adac12062eb77386b5d7ba2
Aman <bhaskarshah4235@gmail.com>
2024-07-18 19:37:21 +0530

    pattern10

--- pattern1.cpp
+++ pattern1.cpp
@@ -162,10 +162,23 @@ void pattern9(int n){
 
 }
 void pattern10(int n){
-    for(int i=0; i<2n-1; i++){
-        
+    // *
+    // **
+    // ***
+    // ****
+    // ***
+    // **
+    // *
+    for(int i=0; i<2*n-1; i++){
+        int star = i;
+        if(i>n) star = 2*n-i;
+        for(int j=1; j<star; j++){
+            cout << "*";
+        }
+    cout << endl;
     }
 }
+
 
 int main(){
     // int t;
@@ -184,5 +197,5 @@ int main(){
 
     int n;
     cin >> n;
-    pattern9(n);
+    pattern10(n);
 }
