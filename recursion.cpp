2a9bcb757419b5b7985d79f61dffbfa10b6f3804
Aman <bhaskarshah4235@gmail.com>
2024-07-28 23:54:20 +0530

    cbasic

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,14 @@
-#include<bits/c++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
+int sum = 0;
 int goal (){
+    cout << sum << " ";
+    sum++;
+    if(sum == 4) return 0;
+    else 
+    cout << sum << endl;
+}
 
+int main(){
+    goal();
 }
