a2522411c57cd221ce3c9846ac4ca769b77b2a2a
Aman <bhaskarshah4235@gmail.com>
2024-08-03 19:42:09 +0530

    recurssion q1

--- recurrProb.cpp
+++ recurrProb.cpp
@@ -2,9 +2,24 @@
 using namespace std;
 
 //print a name "n" times using recursion..
+
+// void fnn(int i,int n){
+//     if(i>n) return;
+//     cout << "Aman" << endl;
+//     fnn(i+1, n);
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     fnn(1,n);
+// }
+
+//print numbers upto "n" {input number} using recursion..
+
 void fnn(int i,int n){
     if(i>n) return;
-    cout << "Aman" << endl;
+    cout << i << endl;
     fnn(i+1, n);
 }
 
