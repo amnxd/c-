3cc5983a05c02bd0d202fd32730010531e1eb89b
Aman <bhaskarshah4235@gmail.com>
2024-08-03 22:06:38 +0530

    uasd

--- recurrProb.cpp
+++ recurrProb.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 //q1 print a name "n" times using recursion..
+/*
+void fnn(int i,int n){
+    if(i>n) return;
+    cout << "Aman" << endl;
+    fnn(i+1, n);
+}
 
-// void fnn(int i,int n){
-//     if(i>n) return;
-//     cout << "Aman" << endl;
-//     fnn(i+1, n);
-// }
-
-// int main(){
-//     int n;
-//     cin >> n;
-//     fnn(1,n);
-// }
+int main(){
+    int n;
+    cin >> n;
+    fnn(1,n);
+}*/
 
 //q2 print numbers upto "n" {input number} in ascending order {1-n} using recursion..
-
+/*
 // void fnn(int i,int n){
 //     if(i>n) return;
 //     cout << i << endl;
@@ -28,21 +28,35 @@ using namespace std;
 //     cin >> n;
 //     fnn(1,n);
 // }
+*/
 
 //q3 print numbers upto "n" {input number} in descending order {n-1} using recursion..
 
-void fnn(int n){
-    if(n==0) return;
-    cout << n << endl;
-    fnn(n-1);
-}
-void fnn1(int i,int n){
-    if(i<1) return;
+// void fnn(int n){
+//     if(n==0) return;
+//     cout << n << endl;
+//     fnn(n-1);
+// }
+// void fnn1(int i,int n){
+//     if(i<1) return;
+//     cout << i << endl;
+//     fnn1(i-1,n);
+// }
+// int main(){
+//     int n;
+//     cin >> n;
+//     fnn1(n,n);
+// }
+
+// q3 print numbers upto "n" {input number} in ascending order {1-n} with backtracking..
+
+void fn(int i,int n){
+    if(i>n) return;
+    fn(i-1,n);
     cout << i << endl;
-    fnn1(i-1,n);
 }
 int main(){
     int n;
     cin >> n;
-    fnn1(n,n);
+    fn(n,n);
 }
