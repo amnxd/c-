d90fab238cf4a05dd0d0726451c68927372cdfd7
Aman <bhaskarshah4235@gmail.com>
2024-08-03 19:39:32 +0530

    recurssion q1

--- recurrProb.cpp
+++ recurrProb.cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void qone(){
-    
+//print a name "n" times using recursion..
+void fnn(int i,int n){
+    if(i>n) return;
+    cout << "Aman" << endl;
+    fnn(i+1, n);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    fnn(1,n);
 }
